bf5a67729d285b2f483172f478f8d9d22f78dab8
Shalhavit-Simcha Cohen <shalhavit@gmail.com>
2017-04-06 00:00:19 -0500

    a3 :: [Fixing W3 Validator 4]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="flex-center position-ref full-height">
         <div class="content">
-            <div class="title m-b-md">
-                <h1>Twitter Inspector</h1>
+            <div>
+                <h1 class="title m-b-md">Twitter Inspector</h1>
             </div>
             <a href="inspector">Begin</a>
         </div>
