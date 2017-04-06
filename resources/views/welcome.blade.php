244346a7a7f144144bec4098bd8a4e7b331c084c
Shalhavit-Simcha Cohen <shalhavit@gmail.com>
2017-04-05 23:58:51 -0500

    a3 :: [Fixing W3 Validator 3]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Twitter Inspector
+                <h1>Twitter Inspector</h1>
             </div>
             <a href="inspector">Begin</a>
         </div>
