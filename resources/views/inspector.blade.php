ccc76be6c832def4b9392de09c566be7e7196638
Shalhavit-Simcha Cohen <shalhavit@gmail.com>
2017-04-06 01:00:49 -0500

    a3 :: [Add {{ csrf_field() }} to both forms]

--- resources/views/inspector.blade.php
+++ resources/views/inspector.blade.php
@@ -10,39 +10,39 @@
 
     <section id="postTweetSection" class="col col-lg-8 col-lg-offset-2">
         <h2 class="text-center">@ShalhavitSimcha timeline</h2>
-        <div class="row">
+            <div class="row">
 
-            <form method="POST" action="{{ route('post.tweet') }}" enctype="multipart/form-data">
-                {{ csrf_field() }}
+                <form method="POST" action="{{ route('post.tweet') }}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
 
-                @if(count($errors))
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.
-                        <br/>
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+                    @if(count($errors))
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.
+                            <br/>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="form-group">
+                        <label>Add Tweet Text:</label>
+                        <textarea class="form-control" name="tweet"></textarea>
                     </div>
-                @endif
 
-                <div class="form-group">
-                    <label>Add Tweet Text:</label>
-                    <textarea class="form-control" name="tweet"></textarea>
-                </div>
+                    <div class="form-group">
+                        <label>Add Multiple Images:</label>
+                        <input type="file" name="images[]" multiple class="form-control">
+                    </div>
 
-                <div class="form-group">
-                    <label>Add Multiple Images:</label>
-                    <input type="file" name="images[]" multiple class="form-control">
-                </div>
+                    <div class="form-group text-center">
+                        <button class="btn btn-success">Post New Tweet</button>
+                    </div>
 
-                <div class="form-group text-center">
-                    <button class="btn btn-success">Post New Tweet</button>
-                </div>
-
-            </form>
-        </div>
+                </form>
+            </div>
     </section>
 
     <section id="getTweetsSection" class="col col-lg-12">
@@ -52,7 +52,7 @@
 
             <div class="row text-center">
                 <form method='GET' action='/inspector'>
-
+                    {{ csrf_field() }}
                     <label for='numTweets'>Maximum number of tweets:</label>
                     <input type='number' min='1' max='20' name='numTweets' id='numTweets' value='{{ $numTweets or '20' }}' required>
 
